cd91ad5c4649e9629d53c52eb8f900c05e32602d
Shantanu Kokate <you@example.com>
2024-10-14 18:39:02 +0530

    set up initial devseops project structure

---
+++ src/components/animate/variants/fade/fadeIn.ts
@@ -0,0 +1,5 @@
+shantanu@linux:~/netflix-DevSecOps/src/components/animate$ ls
+shantanu@linux:~/netflix-DevSecOps/src/components/animate$ mkdir variants/fade
+mkdir: cannot create directory ‘variants/fade’: No such file or directory
+shantanu@linux:~/netflix-DevSecOps/src/components/animate$ mkdir variants/f^Ce
+shantanu@linux:~/netflix-DevSecOps/src/components/animate$ 
---
+++ src/routes/index.tsx
@@ -0,0 +1,31 @@
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: MAIN_PATH.root,
+          element: <Navigate to={`/${MAIN_PATH.browse}`} />,
+        },
+        {
+          path: MAIN_PATH.browse,
+          lazy: () => import("src/pages/HomePage"),
+        },
+        {
+          path: MAIN_PATH.genreExplore,
+          children: [
+            {
+              path: ":genreId",
+              lazy: () => import("src/pages/GenreExplore"),
+            },
+          ],
+        },
+        {
+          path: MAIN_PATH.watch,
+          lazy: () => import("src/pages/WatchPage"),
+        },
+      ],
+    },
+  ]);
+  
+  export default router;
